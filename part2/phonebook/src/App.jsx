a0b953d4a97989beae600c0a56f7da6ed7c1aa0d
elvastra <(none)>
2025-05-21 17:30:26 +0300

    part2c done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm.jsx'
 import PhonebookFilter from './components/PhonebookFilter.jsx'
 import PhonebookNumbers from './components/PhonebookNumbers.jsx'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+
+  // Fetch data from server
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => setPersons(res.data))
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
